1a240b38735f74d6a70fbc57b30958ee29be222a
Le Hai Son <lehaisonmath6@gmail.com>
2019-12-20 10:43:23 +0700

    update new

--- MediaCloudEtcdClient/factory.go
+++ MediaCloudEtcdClient/factory.go
@@ -1,6 +1,6 @@
 package MediaCloudEtcdClient
 
-func NewMediaCloudEtcdClient(ahost, aport string) MediaCloudClientIf {
+func NewPubProfileClient(ahost, aport string) MediaCloudClientIf {
 
 	return &mediacloudclient{
 		host: ahost,
