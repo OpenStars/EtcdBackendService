be1ed7deac60e0faa1afbfff4abffd52559bb207
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-05-09 15:10:51 +0700

    update es

--- ESClientService/esclient.go
+++ ESClientService/esclient.go
@@ -14,15 +14,6 @@ const indexString = `
 		"number_of_shards": 1,
 		"number_of_replicas": 0
 	},
-	"analysis": {
-		"analyzer": {
-		  "analyzer-name": {
-				"type": "custom",
-				"tokenizer": "keyword",
-				"filter": "lowercase"
-		  }
-		}
-	},
 	"mappings":{
 		
 	}
