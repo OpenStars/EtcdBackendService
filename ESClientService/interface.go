09038d390d6df14c3fd666be6a09638c8d19e2f2
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-05-13 09:12:57 +0700

    update search

--- ESClientService/interface.go
+++ ESClientService/interface.go
@@ -1,9 +1,12 @@
 package ESClientService
 
+import elastic "gopkg.in/olivere/elastic.v7"
+
 type ESClientServiceIf interface {
 	PutDataToES(id string, dataJson string) (err error)
 	DeleteIndexES()
 	DeleteDataES(id string)
 	UpdateDataES(id string, mapUpdate map[string]interface{})
 	PutDataToES2(id string, data interface{}) error
+	SearchESByQuery(mapSearch map[string]interface{}, sort map[string]bool) ([]*elastic.SearchHit, error)
 }
