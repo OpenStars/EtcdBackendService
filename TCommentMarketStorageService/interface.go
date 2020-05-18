434494bea1c87363bd74ad59b8bede5f97b74de8
Le Hai Son <sonlh@sonek.vn>
2020-05-18 07:46:27 +0700

    oke

---
+++ TCommentMarketStorageService/interface.go
@@ -0,0 +1,10 @@
+package TCommentStorageService
+
+import "github.com/OpenStars/EtcdBackendService/TCommentMarketStorageService/tcommentstorageservice/thrift/gen-go/OpenStars/Common/TCommentStorageService"
+
+type TCommentStorageServiceIf interface {
+	GetData(idcomment int64) (*TCommentStorageService.TCommentItem, error)
+	PutData(idcomment int64, data *TCommentStorageService.TCommentItem) error
+	RemoveData(idcomment int64) error
+	GetListDatas(listkey []int64) ([]*TCommentStorageService.TCommentItem, error)
+}
