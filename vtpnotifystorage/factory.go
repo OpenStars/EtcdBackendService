46412c7dff03d639b3c1ff18947132a12c680a3a
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-08-31 09:22:25 +0700

    add notify storage

---
+++ vtpnotifystorage/factory.go
@@ -0,0 +1,28 @@
+package vtpcalllogservice
+
+import (
+	"log"
+
+	"github.com/OpenStars/GoEndpointManager"
+)
+
+func NewVTPNotifyStorageService(etcdServers []string, serviceID, defaulHost, defaultPort string) VTPNotifyStorageService {
+	// return sv
+	notifystorageService := &vtpnotifystorage{
+		host:        defaulHost,
+		port:        defaultPort,
+		sid:         serviceID,
+		etcdManager: GoEndpointManager.GetEtcdBackendEndpointManagerSingleton(etcdServers),
+	}
+
+	if notifystorageService.etcdManager == nil {
+		return notifystorageService
+	}
+	err := notifystorageService.etcdManager.SetDefaultEntpoint(serviceID, defaulHost, defaultPort)
+	if err != nil {
+		log.Println("SetDefaultEndpoint sid", serviceID, "err", err)
+		return nil
+	}
+
+	return notifystorageService
+}
