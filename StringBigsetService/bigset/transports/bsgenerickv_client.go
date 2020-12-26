41e2ee7fea88a9fc3bb38512f0e2dd12ff4cca19
Hoang Bui <hoangbm@sonek.vn>
2020-12-26 10:04:58 +0700

    dynamic pass config pool to string bs service

--- StringBigsetService/bigset/transports/bsgenerickv_client.go
+++ StringBigsetService/bigset/transports/bsgenerickv_client.go
@@ -4,35 +4,79 @@ import (
 	"github.com/OpenStars/EtcdBackendService/StringBigsetService/bigset/thrift/gen-go/openstars/core/bigset/generic"
 	thriftpool "github.com/OpenStars/thriftpoolv2"
 	"github.com/apache/thrift/lib/go/thrift"
+	"sync"
 )
+
+type PoolConfig struct {
+	MaxConn     uint32
+	ConnTimeout uint32
+	IdleTimeout uint32
+}
 
 var (
-	bsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
-		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (generic.NewTStringBigSetKVServiceClient(c)) }),
-		thriftpool.DefaultClose)
+	onceInit = &sync.Once{}
+	bsGenericMapPool *thriftpool.MapPool
+	//bsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+	//	thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTStringBigSetKVServiceClient(c) }),
+	//	thriftpool.DefaultClose)
 
-	ibsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
-		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (generic.NewTIBSDataServiceClient(c)) }),
-		thriftpool.DefaultClose)
+	ibsGenericMapPool *thriftpool.MapPool
+	//ibsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+	//	thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTIBSDataServiceClient(c) }),
+	//	thriftpool.DefaultClose)
 )
+
+func InitPool(config PoolConfig)  {
+	onceInit.Do(func() {
+		bsGenericMapPool = thriftpool.NewMapPool(config.MaxConn, config.ConnTimeout, config.IdleTimeout,
+			thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTStringBigSetKVServiceClient(c) }),
+			thriftpool.DefaultClose)
+
+		ibsGenericMapPool = thriftpool.NewMapPool(config.MaxConn, config.ConnTimeout, config.IdleTimeout,
+			thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTIBSDataServiceClient(c) }),
+			thriftpool.DefaultClose)
+	})
+}
 
 //GetBsGenericClient client by host:port
 func GetBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	initBsStringIfNeed()
+
 	client, _ := bsGenericMapPool.Get(aHost, aPort).Get()
 	return client
 }
 func NewGetBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	initBsStringIfNeed()
+
 	client, _ := bsGenericMapPool.NewGet(aHost, aPort).Get()
 	return client
 }
 
 //GetIBsGenericClient client by host:port
 func GetIBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	initBsStringIfNeed()
+
 	client, _ := ibsGenericMapPool.Get(aHost, aPort).Get()
 	return client
 }
 
 func NewGetIBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	initBsStringIfNeed()
+
 	client, _ := ibsGenericMapPool.NewGet(aHost, aPort).Get()
 	return client
+}
+
+func initBsStringIfNeed() {
+	if bsGenericMapPool == nil {
+		onceInit.Do(func() {
+			bsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+				thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTStringBigSetKVServiceClient(c) }),
+				thriftpool.DefaultClose)
+
+			ibsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+				thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return generic.NewTIBSDataServiceClient(c) }),
+				thriftpool.DefaultClose)
+		})
+	}
 }
